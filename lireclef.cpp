2a1345018d0bd9ecfd30394c3ec9193a21d26551
Rado Andrianjanahary <rado.md92@gmail.com>
2014-10-27 19:20:51 +0100

    + fichier

--- lireclef.cpp
+++ lireclef.cpp
@@ -1,5 +1,13 @@
 #include "moteurbd.h"
 
+double DonneeElementaire::operator [](double indexDecimal)
+{
+int index = static_cast<int>(indexDecimal);
+double coef = indexDecimal-index;
+double ajustement=(tab[index+1]-tab[index]) * coef;
+return tab[index]+ajustement;
+}
+
 DonneeElementaire MoteurBD::lireclef(DonneeElementaire clef)
 {
     /* Lecture la "clef". Renvoie la ligne de donnée correspondante à la valeur de la donnée élémentaire. */
